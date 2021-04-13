49093d8255305def1ea76c1f6fb94fd376ace4f0
Daniel Berger <djberg96@gmail.com>
2021-04-13 06:59:57 -0400

    Remove some useless assignments, formatting.

--- lib/crypt/rot13.rb
+++ lib/crypt/rot13.rb
@@ -2,15 +2,13 @@
 
 # The Crypt module serves as a namespace only.
 module Crypt
-
   # The Rot13 class encapsulates methods governing character rotation.
   class Rot13 < String
-
     # The Rot13::Error class is raised if an illegal rotation value is used.
     class Error < ArgumentError; end
 
     # The version of the crypt-rot13 library.
-    VERSION = '1.2.0'.freeze
+    VERSION = '1.2.0'
 
     # Returns a new Rot13 object. The object is a string with the letters
     # each rotated by +degree+.
@@ -33,20 +31,20 @@ module Crypt
 
     def rotate_string(str, degree)
       case degree.modulo(26)
-        when 0
-          raise Error, 'degree must not be a multiple of 26'
-        when 13
-          str = str.tr('a-zA-Z', 'n-za-mN-ZA-M')
-        else
-          str = str.unpack('C' * str.length).map!{ |e|
-            if e >= 97 && e <= 122
-              e = ((e - 97 + degree) % 26) + 97
-            elsif e >= 65 && e <= 90
-              e = ((e - 65 + degree) % 26) + 65
-            else
-              e = e
-            end
-          }.pack('C' * str.length)
+      when 0
+        raise Error, 'degree must not be a multiple of 26'
+      when 13
+        str = str.tr('a-zA-Z', 'n-za-mN-ZA-M')
+      else
+        str = str.unpack('C' * str.length).map! do |e|
+          if e >= 97 && e <= 122
+            ((e - 97 + degree) % 26) + 97
+          elsif e >= 65 && e <= 90
+            ((e - 65 + degree) % 26) + 65
+          else
+            e
+          end
+        end.pack('C' * str.length)
       end
 
       str
